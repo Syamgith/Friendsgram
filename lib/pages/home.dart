4b50dd5b9a87a377d90400b2b34934e7d10b9f68
syamgith <syamgithksudheesh@gmail.com>
2020-05-15 16:33:30 +0530

    made homeui

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isAuth = false;
+  Widget buildAuthScreen() {
+    return Text('Authenticated');
+  }
+  Scaffold buildUnAuthScreen() {
+    return  Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.teal, Colors.purple],)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('Friendsgram', style: TextStyle(fontFamily: 'signatra', fontSize: 90, color: Colors.white ),),
+            GestureDetector(child: Container(
+              height: 60.0, width: 260.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/images/google_signin_button.png'),
+                fit: BoxFit.cover)
+              ),
+            ),),
+          ],
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return isAuth? buildAuthScreen() : buildUnAuthScreen();
   }
 }
